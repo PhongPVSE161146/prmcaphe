75d793333d2d0e272bba5506c522a11c294f2ff0
ngocnhat24 <156098975+ngocnhat24@users.noreply.github.com>
2025-06-26 18:38:51 +0700

    update change password again

--- app/src/main/java/com/example/project_of_me/Change_Password.java
+++ app/src/main/java/com/example/project_of_me/Change_Password.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.project_of_me.DAO.UserDAO;
 import com.example.project_of_me.Utils.PasswordHasher;
