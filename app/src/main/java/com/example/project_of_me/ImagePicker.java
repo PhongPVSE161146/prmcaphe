4b6768e0cf19a2bb67e85552e1436741ea6b1a5b
ngocnhat24 <156098975+ngocnhat24@users.noreply.github.com>
2025-06-28 10:58:45 +0700

    update image

--- app/src/main/java/com/example/project_of_me/ImagePicker.java
+++ app/src/main/java/com/example/project_of_me/ImagePicker.java
@@ -7,12 +7,18 @@ import android.net.Uri;
 import android.provider.MediaStore;
 
 public class ImagePicker {
+    // Hằng số để xác định yêu cầu chọn ảnh từ thư viện
     public static final int PICK_IMAGE_REQUEST = 1;
+    // Hằng số để xác định yêu cầu chụp ảnh từ camera
     public static final int CAMERA_REQUEST_CODE = 2;
 
+    // Phương thức để mở thư viện ảnh cho người dùng chọn ảnh
     public static void chooseImage(Activity activity) {
+        // Tạo Intent với hành động ACTION_PICK để chọn ảnh từ bộ nhớ ngoài
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Chỉ định loại dữ liệu là ảnh
         intent.setType("image/*");
+        // Khởi chạy Intent và chờ kết quả trả về
         activity.startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
